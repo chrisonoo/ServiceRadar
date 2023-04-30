66e19f273ba3dca9f40c642501df701d22225ade
chrisonoo <chris@onoo.no>
2023-04-30 14:13:37 +0200

    feat: add form validation on backend side

--- ServiceRadar.Application/Dtos/WorkshopDto.cs
+++ ServiceRadar.Application/Dtos/WorkshopDto.cs
@@ -3,13 +3,13 @@
 namespace ServiceRadar.Application.Dtos;
 public class WorkshopDto
 {
-    [Required]
-    [StringLength(20, MinimumLength = 2)]
+    [Required(ErrorMessage = "Field is required")]
+    [StringLength(20, MinimumLength = 2, ErrorMessage = "Length: min. 2, max. 20")]
     public string Name { get; set; } = default!;
-    [Required]
+    [Required(ErrorMessage = "Field is required")]
     public string? Description { get; set; }
     public string? About { get; set; }
-    [StringLength(12, MinimumLength = 8)]
+    [StringLength(12, MinimumLength = 8, ErrorMessage = "Length: min. 8, max. 12")]
     public string? PhoneNumber { get; set; }
     public string? Street { get; set; }
     public string? City { get; set; }
