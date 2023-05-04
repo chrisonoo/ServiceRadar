4b2f73c91b8b677e5fac17af3b0ac7bfc63e272d
chrisonoo <chris@onoo.no>
2023-05-04 16:28:41 +0200

    feat: add login and register page

---
+++ ServiceRadar.MVC/Views/Shared/_LoginPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceRadar.MVC.Views.Shared
+{
+    public class _LoginPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
