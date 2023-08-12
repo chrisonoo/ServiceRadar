62e0f3ed123d6720bbcb94bf3d1a5d15216178c4
chrisonoo <chris@onoo.no>
2023-08-12 17:52:52 +0200

    feat: Add modal window and button in edit view

---
+++ ServiceRadar.Application/WorkshopServices/Commands/CreateWorkshopServiceCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ServiceRadar.Application.WorkshopServices.Dtos;
+
+namespace ServiceRadar.Application.WorkshopServices.Commands;
+public class CreateWorkshopServiceCommand : WorkshopServiceDto, IRequest
+{
+    public string WorskhopEncodedName { get; set; } = default!;
+
+}
