d19ab7eb2d9488cdd4c34d6907d5539d07786f90
chrisonoo <chris@onoo.no>
2023-08-19 10:02:55 +0200

    feat: Show related services for a workshop

--- ServiceRadar.MVC/wwwroot/js/Workshop/Edit.js
+++ ServiceRadar.MVC/wwwroot/js/Workshop/Edit.js
@@ -1,4 +1,56 @@
 $(document).ready(function () {
+    const RenderWorkshopServices = (services, container) => {
+        container.empty();
+
+        let servicesTable = '';
+        let iteration = 1;
+
+        for (const service of services) {
+            servicesTable +=
+                `<tr><th scope="row">${iteration}</th>
+                 <td>${service.description}</td>
+                 <td>${service.cost}</td></tr>`
+            iteration++;
+        }
+
+        container.append(
+            `<table class="table">
+                 <thead>
+                     <tr>
+                         <th scope="col">#</th>
+                         <th scope="col">Service</th>
+                         <th scope="col">Price</th>
+                     </tr>
+                 </thead>
+                 <tbody>
+                     ${servicesTable}
+                 </tbody>
+             </table>`
+        )
+    }
+
+    const LoadWorkshopServices = () => {
+        const container = $("#services");
+        const workshopEncodedName = container.data("encodedName");
+
+        $.ajax({
+            url: `/Workshop/${workshopEncodedName}/WorkshopService`,
+            type: 'get',
+            success: function (data) {
+                if (!data.length) {
+                    container.html("There are no services for this workshop");
+                } else {
+                    RenderWorkshopServices(data, container);
+                }
+            },
+            error: function () {
+                toastr["error"]("Something went wrong")
+            }
+        });
+    }
+
+    LoadWorkshopServices();
+
     $("#createWorkshopServiceModal form").submit(function (event) {
         event.preventDefault();
 
