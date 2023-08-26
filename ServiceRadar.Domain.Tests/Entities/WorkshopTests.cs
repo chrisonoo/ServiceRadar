1f0c0fa8b92e01a4f0d4e4812fc0f7db9ad398ec
chrisonoo <chris@onoo.no>
2023-08-26 14:19:40 +0200

    test: Add unit tests for Domain

---
+++ ServiceRadar.Domain.Tests/Entities/WorkshopTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace ServiceRadar.Domain.Entities.Tests;
+
+public class WorkshopTests
+{
+    [Fact]
+    public void EncodeName_ShouldSetEncodedName()
+    {
+        // Arrange
+
+        var workshop = new Workshop();
+        workshop.Name = "Test Workshop";
+
+        // Act
+
+        workshop.EncodeName();
+
+        // Assert
+
+        workshop.EncodedName.Should().Be("test-workshop");
+    }
+
+    [Fact]
+    public void EncodedName_ShouldThrowException_WhenNameIsNull()
+    {
+        // Arrange
+
+        var workshop = new Workshop();
+
+        // Act
+
+        Action action = () => workshop.EncodeName();
+
+        // Assert
+
+        action.Invoking(a => a.Invoke())
+            .Should().Throw<NullReferenceException>();
+    }
+}
